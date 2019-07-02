ab0a3d8fb58391958f94a3eee4585f632ac269fe
Swapnil Buchke <sbuchke085@gmail.com>
2019-07-02 16:34:06 +0530

    start,stop,create,delete instances

--- src/controller/aws.py
+++ src/controller/aws.py
@@ -11,11 +11,45 @@ def aws_dashboard(username):
 
 @aws.route("/<username>/aws/ec2",methods=["GET","POST"])
 def ec2_instances(username):
-    print(username)
-    all_instances = Aws.get_all_ec2_instances(username)
-    print(all_instances)
-    return render_template("/aws/ec2_instances.html",all_instances = all_instances)
+    if request.method == "GET":
+        all_instances = Aws.get_all_ec2_instances(username)
+        return render_template("/aws/ec2_instances.html",all_instances = all_instances)
+    
+    elif request.method == "POST":
+        if request.form["Start"] :
+            start = request.form["Start"]
+            instance_id = start.split(" ")[1]
+            Aws.start(instance_id,username)
 
+        elif request.form["Stop"]:
+            stop = request.form["Stop"]
+            instance_id = stop.split(" ")[1]
+            Aws.stop(instance_id,username)
+
+        elif request.form["Reboot"]:
+            reboot = request.form["Reboot"]
+            instance_id = reboot.split(" ")[1]
+            Aws.reboot(instance_id,username)
+  
+@aws.route("/<username>/aws/ec2/create",methods=["GET","POST"])
+def create_ec2(username):
+    print("hello")
+    Aws.create_ec2_instances(username)
+    return render_template("/aws/test.html")
+
+@aws.route("/<username>/aws/ec2/terminate",methods=["GET","POST"])
+def terminate_ec2(username):
+    print("hello")
+    Aws.terminate_ec2_instance(username)
+    return render_template("/aws/test.html")
+
+
+
+
+
+
+#implement checkboxes
+#@aws.route("/<username>/aws")
 #@aws.route("/<username>/aws/ec2",methods=["post"])
 #def ec2_instances_functions(startstopinstance , username):
 #    all_instances = aws.get_all_ec2_instances(username)
@@ -25,7 +59,7 @@ def ec2_instances(username):
 #        if start=="start "+str(i.id):
 #            instanceid = i.id
         
-#    obj = aws(instanceid) #instanceid is passed through here
+#    obj = Aws(instanceid) #instanceid is passed through here
 #    if(start=="start"):
 #        obj.start();
 #    else:
--- src/services/aws.py
+++ src/services/aws.py
@@ -7,6 +7,10 @@ from botocore.exceptions import ClientError
 from model.aws import Credential
 
 class Aws():
+
+    def __init__(self,instance_id):
+        self.instance_id = instance_id
+
     @classmethod
     def get_session(cls,username):
         username = (username+"@sonata-software.com").lower()
@@ -14,11 +18,11 @@ class Aws():
         ACCESS_KEY = keys[0]
         SECRET_KEY = keys[1]
         print(keys)
-        session = boto3.Session(aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY,region_name='us-east-1')
+        session = boto3.Session(aws_access_key_id=ACCESS_KEY,aws_secret_access_key=SECRET_KEY,region_name='us-east-2')
         return session
     
     @classmethod
-    def get_ec2_resource(cls,session):
+    def get_ec2_resource(cls,username):
         session = cls.get_session(username)
         ec2 = session.resource("ec2")
         return ec2
@@ -26,33 +30,48 @@ class Aws():
     @classmethod
     def get_all_ec2_instances(cls,username):
         ec2 = cls.get_ec2_resource(username)
-        instance_list = list(ec2.instances.all())
-        print(len(instance_list))
+        instance_list = ec2.instances.all()
+        #print(len(instance_list))
+        for instance in ec2.instances.all():
+            print(instance.id , instance.state)
         return instance_list
 
     @classmethod
-    def start_instance(cls,instance_id,username):
+    def start(cls,instance_id,username):
         ec2 = cls.get_ec2_resource(username)
         response = ec2.start_instances(InstanceIds=[instance_id],DryRun=False)
 
     @classmethod
-    def stop_instance(cls,instance_id,username):
+    def stop(cls,instance_id,username):
         ec2 = cls.get_ec2_resource(username)
         response = ec2.stop_instances(InstanceIds=[instance_id],DryRun=False)
 
     @classmethod
-    def create_ec2_instances(cls,image_id,key_name,min_count,max_count,instance_type):
+    def reboot(cls,instance_id,username):
         ec2 = cls.get_ec2_resource(username)
-        instances = ec2.create_instances(ImageId=image_id,MinCount=min_count,MaxCount=max_count,InstanceType=instance_type,keyName=key_name)
-        return instances
+        response = ec2.reboot_instances(InstanceIds=[instance_id],DryRun=False)
 
+    @classmethod
+    def create_ec2_instances(cls,username):
+        ec2 = cls.get_ec2_resource(username)
+        print("hello")
+        instances = ec2.create_instances(ImageId="ami-00c4ae720c30116be",MinCount=1,MaxCount=1,InstanceType="t2.micro",KeyName="WinServer2012")
+        print(instances)
+        #return instances
+    
+    @classmethod
+    def terminate_ec2_instance(cls,username):
+        ec2 = cls.get_ec2_resource(username)
+        #instances = ec2.terminate_instances(InstanceIds="i-02414a7407b7453ab")
+        instances = ec2.instances.filter(InstanceIds = ["i-02414a7407b7453ab"]).terminate()
+        print(instances)
 
-        
+    
         
 
 
     #def Start(self):
-    #    Instance = instance.start(InstanceID = [self.instanceid])
+    #    Instance = EC2.instance.start(InstanceID = [self.instanceid])
     #    #Instance = instance.start(InstanceID.N = [self.instanceid])
     #    ##https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_StartInstances.html
     #    return Instance
